f602d8ffd4dec0d3965a8fa8ae8a46ff6a698246
Jameel Kusugal <jam414@yahoo.com>
2021-01-19 13:24:46 -0500

    changed the message

--- helloLambda.py
+++ helloLambda.py
@@ -7,5 +7,5 @@ def lambda_handler(event, context):
     print("Hello")
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda again!')
+        'body': json.dumps('Hello from Lambda again !!')
     }
