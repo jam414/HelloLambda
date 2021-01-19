e78aab282ed5475a18bd59d5b01551d3d6c61db9
Jameel Kusugal <jam414@yahoo.com>
2021-01-18 22:52:42 -0500

    update file
    remark

--- helloLambda.py
+++ helloLambda.py
@@ -6,6 +6,7 @@ def lambda_handler(event, context):
     # TODO implement
     print(event)
     print("Testing")
+    print("Testing")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
