a3e832045b9a3b76cd3e1806cd0f486d883bd49c
Jameel Kusugal <jam414@yahoo.com>
2021-01-19 13:35:30 -0500

    updated the message again

--- helloLambda.py
+++ helloLambda.py
@@ -4,8 +4,8 @@ import os
 
 def lambda_handler(event, context):
     # TODO implement
-    print("Hello")
+    message = "<h1>Hello World !</h1>"
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda again and again!!')
+        'body': message
     }
